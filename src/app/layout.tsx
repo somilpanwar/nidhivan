d127d87e0586ab5307e8b0db84643398b19deb9c
somil panwar <122174329+somilpanwar@users.noreply.github.com>
2025-04-18 11:43:17 +0530

    add: added vercal analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import Navbar from "@/components/Navbar";
 import { Toaster } from "@/components/ui/sonner";
+import { Analytics } from "@vercel/analytics/react"
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -34,6 +35,7 @@ export default function RootLayout({
           <Navbar />
         {children}
         <Toaster />
+      <Analytics />
       </body>
         </ClerkProvider>
     </html>
