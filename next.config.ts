6055ee8bfd17e9d0d5454483f06242c288370846
somil panwar <122174329+somilpanwar@users.noreply.github.com>
2025-04-22 00:40:50 +0530

    fix: remove next-analyzer

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,8 @@
 import type { NextConfig } from "next";
 
-import withBundleAnalyzer from '@next/bundle-analyzer';
-
-const bundleAnalyzer = withBundleAnalyzer({
-  enabled: process.env.ANALYZE === 'true',
-});
 const nextConfig: NextConfig = {
   /* config options here */
   
 };
 
-export default  bundleAnalyzer(nextConfig);
+export default  nextConfig;
